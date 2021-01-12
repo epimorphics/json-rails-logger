c09399d2623deb245f7ea424774b0536eb7c7660
bogdan <marc.bogdan96@yahoo.com>
2021-01-12 14:44:22 +0000

    Required files and dependencies added to main file

--- lib/json_rails_logger.rb
+++ lib/json_rails_logger.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'lograge'
+require 'json_rails_logger/railtie' if defined?(Rails)
+
+require 'json_rails_logger/error.rb'
+require 'json_rails_logger/logger.rb'
+require 'json_rails_logger/version.rb'
+
+require 'json_rails_logger/formatter/json.rb'
+
 # A custom rails logger that outputs json instead of raw text
 module JsonRailsLogger
   def self.setup(app); end
