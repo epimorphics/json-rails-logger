0a6f1455e231eaa0367004099e507fdbb568d68f
bogdan <marc.bogdan96@yahoo.com>
2021-01-12 14:52:33 +0000

    Added railtie used mainly for the lograge config

---
+++ lib/json_rails_logger/railtie.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module JsonRailsLogger
+  # This class is used to configure and setup lograge, as well as our gem
+  class Railtie < Rails::Railtie
+    config.lograge.formatter = Lograge::Formatters::Json.new
+    config.lograge.custom_options = lambda do |event|
+      {
+        exception: event.payload[:exception],
+        exception_object: event.payload[:exception_object]
+      }
+    end
+
+    config.after_initialize do |app|
+      JsonRailsLogger.setup(app) if JsonRailsLogger.enabled?(app)
+      Lograge.setup(app) if JsonRailsLogger.enabled?(app)
+    end
+  end
+end
