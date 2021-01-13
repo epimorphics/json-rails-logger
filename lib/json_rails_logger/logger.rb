28d13f903b680eafbdd9d1890d8daf1b4b87bdb5
bogdan <marc.bogdan96@yahoo.com>
2021-01-13 16:26:45 +0000

    Used the splat operator for logger args instead

--- lib/json_rails_logger/logger.rb
+++ lib/json_rails_logger/logger.rb
@@ -3,17 +3,15 @@
 module JsonRailsLogger
   # The custom logger class that sets up our formatter
   class Logger < ::Logger
-    def initialize(logdev, shift_age = 0, shift_size = 1_048_576, level: DEBUG,
-                   progname: nil, formatter: nil, datetime_format: nil,
-                   binmode: false, shift_period_suffix: '%Y%m%d')
+    # List of all the arguments with their default values:
+    # logdev, shift_age = 0, shift_size = 1_048_576, level: DEBUG,
+    # progname: nil, formatter: nil, datetime_format: nil,
+    # binmode: false, shift_period_suffix: '%Y%m%d'
+    def initialize(*args)
       @formatter = JsonRailsLogger::Formatter::Json.new
-      @formatter.datetime_format = datetime_format
-      super(logdev, shift_age, shift_size, level: level,
-                                           progname: progname,
-                                           formatter: @formatter,
-                                           datetime_format: datetime_format,
-                                           binmode: binmode,
-                                           shift_period_suffix: shift_period_suffix)
+      @formatter.datetime_format = '%Y-%m-%d %H:%M:%S'
+
+      super(*args, formatter: @formatter)
     end
   end
 end
