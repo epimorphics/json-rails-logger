cd0c68b935f11d776d2c8690f8efcd0c2acddfee
bogdan <marc.bogdan96@yahoo.com>
2021-01-15 15:12:46 +0000

    Added method for processing arguments

--- lib/json_rails_logger/formatter/json.rb
+++ lib/json_rails_logger/formatter/json.rb
@@ -4,14 +4,14 @@ module JsonRailsLogger
   module Formatter
     # This class is the json formatter for our logger
     class Json < ::Logger::Formatter
-      def call(severity, timestamp, _progname, raw_msg)
-        msg = normalize_message(raw_msg)
+      def call(severity, timestamp, progname, raw_msg)
+        args = process_arguments(severity, timestamp, progname, raw_msg)
 
         payload = {
-          level: severity,
-          timestamp: format_datetime(timestamp),
+          level: args[:severity],
+          timestamp: args[:timestamp],
           environment: ::Rails.env,
-          message: msg
+          message: args[:msg]
         }
 
         "#{payload.to_json}\n"
@@ -19,6 +19,18 @@ module JsonRailsLogger
 
       private
 
+      def process_arguments(severity, timestamp, progname, raw_msg)
+        sev = process_severity(severity)
+        timestp = process_timestamp(timestamp)
+        new_msg = process_message(raw_msg)
+
+        {
+          severity: sev,
+          timestamp: timestp,
+          progname: progname,
+          msg: new_msg
+        }
+      end
       def normalize_message(raw_msg)
         return raw_msg unless raw_msg.is_a?(String)
 
