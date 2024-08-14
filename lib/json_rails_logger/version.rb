0c187f53937eea089e1eedd31908836fad2ddcfc
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-08-14 16:49:38 +0100

    docs: Updated version cadence
    incremented the patch digit to `4` as well as added the `.rc01` suffix

--- lib/json_rails_logger/version.rb
+++ lib/json_rails_logger/version.rb
@@ -3,7 +3,7 @@
 module JsonRailsLogger
   MAJOR = 1
   MINOR = 0
-  PATCH = 3
-  SUFFIX = nil
-  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}"
+  PATCH = 4
+  SUFFIX = 'rc01'
+  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
 end
