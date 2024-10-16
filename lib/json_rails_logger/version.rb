94b7738e7337db62b9579683c5c54757a94a5fb9
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-10-16 15:38:27 +0100

    Auto stash before merge of "spike/update-publish-action" and "origin/main"

--- lib/json_rails_logger/version.rb
+++ lib/json_rails_logger/version.rb
@@ -2,15 +2,8 @@
 
 module JsonRailsLogger
   MAJOR = 1
-<<<<<<< Updated upstream
   MINOR = 1
   PATCH = 0
   SUFFIX = nil
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
-=======
-  MINOR = 0
-  PATCH = 4
-  SUFFIX = '-rc01'
-  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "#{SUFFIX}"}"
->>>>>>> Stashed changes
 end
