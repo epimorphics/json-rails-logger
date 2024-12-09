85e4d9c4c0ea50b51d4fadb66c4e53e3fae63a02
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-12-09 12:47:45 +0000

    build: updated gem version to 2.0.0

--- lib/json_rails_logger/version.rb
+++ lib/json_rails_logger/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module JsonRailsLogger
-  MAJOR = 1
-  MINOR = 1
-  PATCH = 1
+  MAJOR = 2
+  MINOR = 0
+  PATCH = 0
   SUFFIX = nil
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
 end
