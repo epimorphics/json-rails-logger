3888d39ba6ca91d2259d72e80291f4435dd468df
Jon R. Humphrey <jon@humphrey.me.uk>
2026-03-17 08:14:28 +0000

    chore(release): bump version to 3.0.0

--- lib/json_rails_logger/version.rb
+++ lib/json_rails_logger/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module JsonRailsLogger
-  MAJOR = 2
-  MINOR = 3
+  MAJOR = 3
+  MINOR = 0
   PATCH = 0
   SUFFIX = nil
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}".freeze
